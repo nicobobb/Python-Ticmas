b7538ed0343110d13ac6b40488301073c3685f9c
Nicolás Bobb <nicobobb@gmail.com>
2023-08-15 21:11:24 -0300

    Primeros ejercicios

---
+++ Ejercicio1.py
@@ -0,0 +1,6 @@
+edad = int(input("Ingrese su edad: \n"))
+
+if edad < 18 :
+    print("Eres menor de edad")
+else:
+    print("Eres mayor de edad")
---
+++ Ejercicio4.py
@@ -0,0 +1,9 @@
+num1 = int(input("Ingrese un número: "))
+num2 = int(input("Ingrese otro número: "))
+
+if num1<num2:
+    print("El primer número es más grande")
+elif num2< num1:
+    print("El segundo es menor")
+else:
+    print("Los números son iguales")
