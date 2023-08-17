f1cdba6d3af4e152dcc8853b6e997b40ef935f22
Nicolás Bobb <nicobobb@gmail.com>
2023-08-17 00:22:50 -0300

    Update Ejercicio6-Listas.py

--- Ejercicio6-Listas.py
+++ Ejercicio6-Listas.py
@@ -10,3 +10,25 @@ for value in lista:
 numeros = [2, 1, 9, 5, 4, 3, 10]
 numeros.sort()
 print(numeros)
+
+
+my_dic = {
+    'nombre': 'Juan',
+    # 'edad': 46
+}
+
+print(my_dic['nombre'])
+print(my_dic.get('edad', 'No tiene edad'))
+
+my_set = set([1, 2, 3])
+
+my_set.add(2)  # No se agrega el 2 porque ya existe.
+my_set.add(22)
+my_set.remove(2)
+print(my_set)
+
+my_set2 = set([3, 4, 5])
+
+union_de_sets = my_set.union(my_set2)
+
+print(union_de_sets)
