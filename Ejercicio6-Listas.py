ce295bf8db580851a68138d381f9fd86c49860c2
Nicolás Bobb <nicobobb@gmail.com>
2023-08-16 23:59:59 -0300

    Update Ejercicio6-Listas.py

---
+++ Ejercicio6-Listas.py
@@ -0,0 +1,12 @@
+lista = ['Hola', 'Chau', 'Nos vemos']
+print(lista[1])
+
+lista.append('Cachichen')
+lista.insert(1, 'Estoy')
+
+for value in lista:
+    print(value)
+
+numeros = [2, 1, 9, 5, 4, 3, 10]
+numeros.sort()
+print(numeros)
